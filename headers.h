2a0fabe1769c95206c047a5be16ef349fa4b2d41
Aryan R Chugh <aaryanrchugh@gmail.com>
2024-10-22 19:12:55 +0530

    initial_commit

---
+++ headers.h
@@ -0,0 +1,11 @@
+#ifndef __HEADERS_H
+#define __HEADERS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdbool.h>
+#include <string.h>
+
+#endif
