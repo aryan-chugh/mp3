075e337204d3c6edf54663d9d622de04edf61067
Aryan R Chugh <aaryanrchugh@gmail.com>
2024-10-22 19:32:08 +0530

    output_functions added

--- headers.h
+++ headers.h
@@ -8,4 +8,12 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define WHITE   "\033[37m"
+#define PINK    "\033[35m"
+#define YELLOW  "\033[33m"
+#define BOLD    "\033[1m"
+#define RESET   "\033[0m"
+
 #endif
